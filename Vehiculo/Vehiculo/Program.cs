9b0486df5da8a245c0432520fa0e2e637c2f9efd
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-02 12:56:00 -0500

    Proyecto donde se practica la herencia

---
+++ Vehiculo/Vehiculo/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("probando el avion");
+
+Vehiculo.Avion miAvion = new Vehiculo.Avion();
+miAvion.ArrancaMotor("tracatra");
+
+miAvion.Despegar();
+miAvion.Conducir();
+miAvion.Aterrizar();
+miAvion.PararMotor("ploff");
+
+Console.WriteLine();
+
+Console.WriteLine("Probando el coche");
+
+Vehiculo.Coche miCoche = new Vehiculo.Coche();
+miCoche.ArrancaMotor("grrrrum grummm");
+miCoche.Conducir();
+miCoche.Acelerar();
+miCoche.Frenar();
+miCoche.PararMotor("bluuuuff");
+
+Console.WriteLine("Polimorfismo en accion!");
+
+Vehiculo.Vehiculo miVehiculo = miCoche;
+miVehiculo.Conducir();
+miVehiculo = miAvion;
+miVehiculo.Conducir();
---
+++ Vehiculo/Vehiculo/Vehiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculo
+{
+    internal class Vehiculo
+    {
+        public void ArrancaMotor(string sonidoAlArrancar)
+        {
+            Console.WriteLine($"Arrancar el motor: {sonidoAlArrancar}");
+        }
+
+        public void PararMotor(string sonidoAlParar)
+        {
+            Console.WriteLine($"Parando el motor: {sonidoAlParar}");
+        }
+
+        public virtual void Conducir()
+        {
+            Console.WriteLine("Este es el metodo generico para el metodo conducir");
+        }
+
+    }
+}
