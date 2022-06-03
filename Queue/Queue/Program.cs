d3fd6f6df3a2690dc2a09f75e2fb404488a44aa7
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-03 16:10:09 -0500

    Ejemplo de colas

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Queue <int> numeros = new Queue<int> ();
+
+//Agregando elementos a la cola
+
+foreach (int numero in new int[5] {2, 4, 5, 8, 10})
+{
+    numeros.Enqueue(numero);
+}
+
+//Recorriendo la cola
+
+Console.WriteLine("Recorriendo el Queue: ");
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+Console.WriteLine();
+//Eliminando elementos de la cola
+
+Console.WriteLine("Eliminando los elementos: ");
+numeros.Dequeue();
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
