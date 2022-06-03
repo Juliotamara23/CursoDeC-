cf72fd174c72705946a891569a823c74ddd4b580
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-03 13:14:38 -0500

    Uso de Destructores en un programa lector de archivos

---
+++ Destructores/Destructores/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ManejoArchivos miArchivo = new ManejoArchivos();
+miArchivo.mensaje();
+
+class ManejoArchivos{
+
+    StreamReader? archivo = null;
+    int contador = 0;
+    string linea;
+
+    public ManejoArchivos()
+    {
+        archivo = new StreamReader(@"C:\Users\spede\OneDrive\Escritorio\texto.txt");
+
+        while ((linea = archivo.ReadLine()) !=null)
+        {
+            Console.WriteLine(linea);
+
+            contador++;
+        }
+    }
+
+    public void mensaje()
+    {
+        Console.WriteLine("Hay {0} lineas", contador);
+    }
+
+    ~ManejoArchivos(){ archivo.Close(); }
+}
