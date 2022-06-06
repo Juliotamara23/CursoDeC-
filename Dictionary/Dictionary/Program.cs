d26baf0a89016a4edfefa4b9bfe18d86c8fb78a1
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-06 09:28:22 -0500

    Ejemplo de uso de dictionary

---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Dictionary<string, int> edades = new Dictionary<string, int>();
+
+// Rellenar el diccionario
+
+edades.Add("Juan", 18);
+
+edades.Add("Diandra", 35);
+
+edades["Maria"] = 25;
+
+edades["Antonio"] = 29;
+
+foreach (KeyValuePair<string, int> persona in edades)
+{
+    Console.WriteLine("Nombre {0}, Edad: {1}", persona.Key, persona.Value);
+}
