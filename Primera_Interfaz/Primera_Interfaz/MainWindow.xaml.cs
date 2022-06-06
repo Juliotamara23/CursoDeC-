8d916b085eff94c51b386e98e8326558a9a27131
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-06 13:15:06 -0500

    Ejemplo de uso de interfaz grafica

---
+++ Primera_Interfaz/Primera_Interfaz/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Primera_Interfaz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Grid grid = new Grid();
+
+            this.Content = grid;
+
+            Button miBoton = new Button();
+
+            WrapPanel wrapPanel = new WrapPanel();
+
+            TextBlock txt1 = new TextBlock();
+
+            txt1.Text = "Click";
+
+            wrapPanel.Children.Add(txt1);
+
+            TextBlock txt2 = new TextBlock();
+
+            txt2.Text = "Enviar";
+
+            wrapPanel.Children.Add(txt2);
+
+            TextBlock txt3 = new TextBlock();
+
+            txt3.Text = "Dale";
+
+            wrapPanel.Children.Add(txt3);
+
+            miBoton.Content = wrapPanel;
+
+            grid.Children.Add(miBoton);
+        }
+    }
+}
