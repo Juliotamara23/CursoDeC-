e5b97533169d9eea2ef53372c99cfcb7939c03d1
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-06 12:38:47 -0500

    Ejemplo de uso de expresiones regulares

---
+++ ExpresionesRegulares/ExpresionesRegulares/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+string txt = "curso@pildorasinformaticas.es";
+
+string re1 = ",*?";
+string re2 = "@";
+string re3 = ".*?";
+string re4 = "\\.";
+
+
+Regex r = new Regex(re1 + re2 + re3 + re4, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+Match match = r.Match(txt);
+
+if (match.Success)
+{
+    Console.WriteLine("Se han encontrado la pagina");
+}
+else
+{
+    Console.WriteLine("No se han encontrado la pagina ");
+}
