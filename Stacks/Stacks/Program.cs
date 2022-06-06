6dccd3565c96929fa616a1ec430819a8013b7461
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-06 08:48:38 -0500

    Ejemplo de uso de Stacks

---
+++ Stacks/Stacks/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Stack <int> numeros = new Stack<int>();
+
+//Agregando elementos a la Stack
+
+foreach (int numero in new int[5] { 2, 4, 5, 8, 10 })
+{
+    numeros.Push(numero);
+}
+
+//Recorriendo la cola
+
+Console.WriteLine("Recorriendo el Stack: ");
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
+
+Console.WriteLine();
+//Eliminando elementos de la cola
+
+Console.WriteLine("Eliminando los elementos del stack: ");
+numeros.Pop();
+
+foreach (int numero in numeros)
+{
+    Console.WriteLine(numero);
+}
