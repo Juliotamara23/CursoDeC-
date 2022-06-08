25624531e50a70866056724fe2b8ffe2ed449ed9
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-08 09:33:08 -0500

    Ejemplo de como mostrar relacion de datos en diferentes tablas en la base de datos

--- ConexionGestionPedidos/ConexionGestionPedidos/MainWindow.xaml.cs
+++ ConexionGestionPedidos/ConexionGestionPedidos/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace ConexionGestionPedidos
 {
@@ -30,13 +31,60 @@ namespace ConexionGestionPedidos
                 ["ConexionGestionPedidos.Properties.Settings.GestionPedidosConnectionString"].ConnectionString;
 
             miConexionSql = new SqlConnection(miConexion);
+
+            MuestraClientes();
         }
 
         private void MuestraClientes()
         {
             string consulta = "Select * from cliente";
+
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(consulta, miConexionSql);
+
+            using (miAdaptadorSql)
+            {
+                DataTable clienteTabla = new DataTable();
+
+                miAdaptadorSql.Fill(clienteTabla);
+
+                listaClientes.DisplayMemberPath = "nombre";
+                listaClientes.SelectedValuePath = "Id";
+                listaClientes.ItemsSource = clienteTabla.DefaultView;
+
+            }
+        }
+
+        private void MuestraPedidos()
+        {
+            string consulta = "Select * from Pedido p inner join cliente c on c.ID=p.cCliente " +
+                "where C.ID=@ClienteId";
+
+            SqlCommand sqlComando = new SqlCommand(consulta, miConexionSql);
+
+            SqlDataAdapter miAdaptadorSql = new SqlDataAdapter(sqlComando);
+
+            using (miAdaptadorSql)
+            {
+
+                sqlComando.Parameters.AddWithValue("@ClienteId", listaClientes.SelectedValue);
+
+                DataTable pedidosTabla = new DataTable();
+
+                miAdaptadorSql.Fill(pedidosTabla);
+
+                pedidosCliente.DisplayMemberPath = "fechaPedido";
+                pedidosCliente.SelectedValuePath = "Id";
+                pedidosCliente.ItemsSource = pedidosTabla.DefaultView;
+
+            }
         }
 
         SqlConnection miConexionSql;
+
+        private void listaClientes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //MessageBox.Show("Has hecho click en un cliente");
+            MuestraPedidos();
+        }
     }
 }
