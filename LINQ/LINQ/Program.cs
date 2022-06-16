fd9c6323f1c903d8a1f40f20219d06fe7f0d475b
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-16 11:53:42 -0500

    Ejemplo de uso de LINQ

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+// See https://aka.ms/new-console-template for more information
+
+int[] ValoresNumericos = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine("Numeros pares: ");
+
+//list<int> numerosPares = new list<int>();
+
+//foreach (int i in valoresnumericos)
+//{
+//    if (i % 2 == 0)
+//    {
+//        numerospares.add(i);
+//    }
+//}
+
+//foreach (int i in numerosPares)
+//{
+//    console.writeline(i);
+//}
+
+IEnumerable<int> numerosPares = from numero in ValoresNumericos where numero%2 ==0 select numero;
+
+foreach (int i in numerosPares)
+{
+    Console.WriteLine(i);
+}
