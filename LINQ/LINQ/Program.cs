6328964b561a435b5571c3cf7d7b6cc733b5dd8d
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-16 12:21:45 -0500

    Ejemplo de consultas de objetos con LINQ

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -2,28 +2,68 @@
 
 // See https://aka.ms/new-console-template for more information
 
-int[] ValoresNumericos = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+ControlEmpresaEmpleados ce = new ControlEmpresaEmpleados();
+ce.getCEO();
 
-Console.WriteLine("Numeros pares: ");
-
-//list<int> numerosPares = new list<int>();
-
-//foreach (int i in valoresnumericos)
-//{
-//    if (i % 2 == 0)
-//    {
-//        numerospares.add(i);
-//    }
-//}
-
-//foreach (int i in numerosPares)
-//{
-//    console.writeline(i);
-//}
-
-IEnumerable<int> numerosPares = from numero in ValoresNumericos where numero%2 ==0 select numero;
-
-foreach (int i in numerosPares)
+class ControlEmpresaEmpleados
 {
-    Console.WriteLine(i);
+    public ControlEmpresaEmpleados()
+    {
+        listaEmpleado = new List<Empleado>();
+        listaEmpresas = new List<Empresa>();
+
+        listaEmpresas.Add(new Empresa { Id = 1, Nombre = "Google" });
+        listaEmpresas.Add(new Empresa { Id = 2, Nombre = "Pildoras Informaticas" });
+
+        listaEmpleado.Add(new Empleado { Id = 1, Nombre = "Sergey Brin", Cargo = "CEO", EmpresaId = 1, Salario = 1500});
+        listaEmpleado.Add(new Empleado { Id = 2, Nombre = "Juan Diaz", Cargo = "CEO", EmpresaId = 2, Salario = 1500 });
+        listaEmpleado.Add(new Empleado { Id = 3, Nombre = "Larry Page", Cargo = "Co-CEO", EmpresaId = 1, Salario = 1400 });
+        listaEmpleado.Add(new Empleado { Id = 4, Nombre = "Irina Shayk", Cargo = "Co-CEO", EmpresaId = 2, Salario = 1400 });
+    }
+
+    public void getCEO()
+    {
+        IEnumerable<Empleado> ceos = from Empleado in listaEmpleado where Empleado.Cargo == "CEO" select Empleado;
+
+        foreach (Empleado empleado1 in ceos)
+        {
+            empleado1.getDatosEmpleado();
+        }
+    }
+
+    public List<Empresa> listaEmpresas;
+    public List<Empleado> listaEmpleado;
+}
+
+class Empresa
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; }
+
+    public void getDatosEmpresa()
+    {
+        Console.WriteLine("Empresa  {0} con Id {1}", Nombre, Id);
+    }
+}
+
+class Empleado
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; }
+
+    public string Cargo { get; set; }
+
+    public double Salario { get; set; }
+
+    //Clase foranea
+
+    public int EmpresaId { get; set; }
+
+    public void getDatosEmpleado()
+    {
+        Console.WriteLine("Empresa  {0} con Id {1}, cargo {2} con salario {3} y " +
+            "pertenece a la empresa {4}", Nombre, Id, Cargo, Salario, EmpresaId);
+    }
 }
