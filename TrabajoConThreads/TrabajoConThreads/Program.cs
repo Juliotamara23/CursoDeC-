956c511a8cfd2e102371f7534beff6438a99ce59
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-21 11:11:55 -0500

    Ejemplo de uso de Threads

---
+++ TrabajoConThreads/TrabajoConThreads/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Thread t = new Thread(MetodoSaludo);
+t.Start();
+
+Thread t2 = new Thread(MetodoSaludo);
+t2.Start();
+
+Console.WriteLine("Hola alumnos desde thread (0 hilo) 1");
+Thread.Sleep(500);
+Console.WriteLine("Hola alumnos desde thread (0 hilo) 1");
+Thread.Sleep(500);
+Console.WriteLine("Hola alumnos desde thread (0 hilo) 1");
+Thread.Sleep(500);
+Console.WriteLine("Hola alumnos desde thread (0 hilo) 1");
+Thread.Sleep(500);
+Console.WriteLine("Hola alumnos desde thread (0 hilo) 1");
+
+static void MetodoSaludo()
+{
+    Console.WriteLine("Hola alumnos desde thread (0 hilo) 2");
+    Thread.Sleep(500);
+    Console.WriteLine("Hola alumnos desde thread (0 hilo) 2");
+    Thread.Sleep(500);
+    Console.WriteLine("Hola alumnos desde thread (0 hilo) 2");
+    Thread.Sleep(500);
+    Console.WriteLine("Hola alumnos desde thread (0 hilo) 2");
+    Thread.Sleep(500);
+    Console.WriteLine("Hola alumnos desde thread (0 hilo) 2");
+}
