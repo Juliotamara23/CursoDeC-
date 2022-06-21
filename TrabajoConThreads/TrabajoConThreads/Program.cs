b39a2098416641509d08bc64c1892eeb37177d96
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-21 13:11:21 -0500

    Ejemplo de uso de threads con la clase TaskCompletionSource para determinar las tareas completadas o no

--- TrabajoConThreads/TrabajoConThreads/Program.cs
+++ TrabajoConThreads/TrabajoConThreads/Program.cs
@@ -31,66 +31,105 @@
 //    Console.WriteLine("Hola alumnos desde thread (0 hilo) 2");
 //}
 
-CuentaBancaria CuentaFamilia = new CuentaBancaria(2000);
+//CuentaBancaria CuentaFamilia = new CuentaBancaria(2000);
 
-Thread[] hilosPersonas = new Thread[15];
+//Thread[] hilosPersonas = new Thread[15];
 
-for (int i = 0; i < 15; i++)
+//for (int i = 0; i < 15; i++)
+//{
+//    Thread t = new Thread(CuentaFamilia.VamosRetirarEfectivo);
+
+//    t.Name = i.ToString();
+
+//    hilosPersonas[i] = t;
+//}
+
+//for (int i = 0; i < 15; i++)
+//{
+//    hilosPersonas[i].Start();
+
+//    hilosPersonas[i].Join(); //Sincronizar el hilo
+//}
+
+//class CuentaBancaria
+//{
+
+//    private Object bloquearSaldoPositivo = new Object();
+
+//    double Saldo {set; get;}
+
+//    public CuentaBancaria(double Saldo)
+//    {
+//        this.Saldo = Saldo;
+//    }
+
+//    public double RetirarEfectivo(double cantidad)
+//    {
+//        //lock(bloquearSaldoPositivo)  //Bloquear el hilo
+//        //{
+//            if ((Saldo - cantidad) < 0)
+//            {
+//                Console.WriteLine($"Lo siento queda {Saldo} en la cuenta. Hilo:{Thread.CurrentThread.Name}");
+//                return 0;
+//            }
+
+//            if (Saldo >= cantidad)
+//            {
+//                Console.WriteLine("Retirado {0} y queda {1} en la cuenta {2}", cantidad, (Saldo - cantidad), Thread.CurrentThread.Name);
+//                Saldo -= cantidad;
+//            }
+
+//            return Saldo; 
+//        //}
+//    }
+
+//    public void VamosRetirarEfectivo()
+//    {
+
+//        Console.WriteLine("Esta sacando dinero el hilo: {0}", Thread.CurrentThread.Name);
+
+//        for (int i = 0; i < 4; i++)
+//        {
+//            RetirarEfectivo(500); 
+//        }
+//    }
+//}
+
+var tareaTerminada = new TaskCompletionSource<bool>();
+
+var hilo1 = new Thread(() =>
 {
-    Thread t = new Thread(CuentaFamilia.VamosRetirarEfectivo);
-
-    t.Name = i.ToString();
-
-    hilosPersonas[i] = t;
-}
-
-for (int i = 0; i < 15; i++)
-{
-    hilosPersonas[i].Start();
-
-    hilosPersonas[i].Join(); //Sincronizar el hilo
-}
-
-class CuentaBancaria
-{
-
-    private Object bloquearSaldoPositivo = new Object();
-
-    double Saldo {set; get;}
-    
-    public CuentaBancaria(double Saldo)
+    for (int i = 0; i < 5; i++)
     {
-        this.Saldo = Saldo;
+        Console.WriteLine("Hilo 1");
+        Thread.Sleep(1000);
     }
 
-    public double RetirarEfectivo(double cantidad)
+    tareaTerminada.TrySetResult(true);
+});
+
+var hilo2 = new Thread(() =>
+{
+    for (int i = 0; i < 5; i++)
     {
-        //lock(bloquearSaldoPositivo)  //Bloquear el hilo
-        //{
-            if ((Saldo - cantidad) < 0)
-            {
-                Console.WriteLine($"Lo siento queda {Saldo} en la cuenta. Hilo:{Thread.CurrentThread.Name}");
-                return 0;
-            }
-
-            if (Saldo >= cantidad)
-            {
-                Console.WriteLine("Retirado {0} y queda {1} en la cuenta {2}", cantidad, (Saldo - cantidad), Thread.CurrentThread.Name);
-                Saldo -= cantidad;
-            }
-
-            return Saldo; 
-        //}
+        Console.WriteLine("Hilo 2");
+        Thread.Sleep(1000);
     }
+});
 
-    public void VamosRetirarEfectivo()
+var hilo3 = new Thread(() =>
+{
+    for (int i = 0; i < 5; i++)
     {
-
-        Console.WriteLine("Esta sacando dinero el hilo: {0}", Thread.CurrentThread.Name);
-
-        for (int i = 0; i < 4; i++)
-        {
-            RetirarEfectivo(500); 
-        }
+        Console.WriteLine("Hilo 3");
+        Thread.Sleep(1000);
     }
-}
+});
+
+hilo1.Start();
+
+var resultado = tareaTerminada.Task.Result;
+
+hilo2.Start();
+
+hilo3.Start();
