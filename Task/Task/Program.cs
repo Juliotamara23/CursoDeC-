7b3316f559d61f19fdd8db8323f20d83852dedd5
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-22 11:09:27 -0500

    Ejemplo de Task usando los metodos wait(), waitAll() y WaitAny()

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -3,8 +3,8 @@
 //Task tarea = new Task(EjecutarTarea);
 //tarea.Start();
 
-Task tarea = Task.Run(() => EjecutarTarea());
-Task tarea2 = tarea.ContinueWith(EjecutarOtraTarea);
+//Task tarea = Task.Run(() => EjecutarTarea());
+//Task tarea2 = tarea.ContinueWith(EjecutarOtraTarea);
 
 //Task tarea2 = new Task(() =>
 //{
@@ -20,28 +20,65 @@ Task tarea2 = tarea.ContinueWith(EjecutarOtraTarea);
 
 //tarea2.Start();
 
+RalizarTodasTareas();
+
 Console.ReadLine();
+
+static void RalizarTodasTareas()
+{
+    var tarea1 = Task.Run(() =>
+    {
+        EjecutarTarea();
+    });
+
+    tarea1.Wait();
+
+    var tarea2 = Task.Run(() =>
+    {
+        EjecutarTarea2();
+    });
+
+    tarea2.Wait();
+
+    var tarea3 = Task.Run(() =>
+    {
+        EjecutarTarea3();
+    });
+
+}
 
 static void EjecutarTarea()
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 5; i++)
     {
         var miThread = Thread.CurrentThread.ManagedThreadId;
 
         Thread.Sleep(1000);
 
-        Console.WriteLine("Esta vuelta de bucle corresponde al Thread: "+miThread);
+        Console.WriteLine("Esta vuelta de bucle corresponde a la tarea 1");
     }
 }
 
-static void EjecutarOtraTarea(Task obj)
+static void EjecutarTarea2()
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 5; i++)
     {
         var miThread = Thread.CurrentThread.ManagedThreadId;
 
-        Thread.Sleep(1000);
+        Thread.Sleep(500);
 
-        Console.WriteLine("Esto Es otra tarea y esta vuelta de bucle corresponde al Thread: " + miThread);
+        Console.WriteLine("Esta vuelta de bucle corresponde a la tarea 2");
+    }
+}
+
+static void EjecutarTarea3()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        var miThread = Thread.CurrentThread.ManagedThreadId;
+
+        Thread.Sleep(500);
+
+        Console.WriteLine("Esta vuelta de bucle corresponde a la tarea 3");
     }
 }
