dde3f2ae073e58347db641f2c61cb461ca04fdb3
Julio Tamara <julio.tamara@cecar.edu.co>
2022-06-22 10:49:32 -0500

    Ejemplo de como crear Task de forma consecutiva

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -1,32 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Task tarea = new Task(EjecutarTarea);
-tarea.Start();
+//Task tarea = new Task(EjecutarTarea);
+//tarea.Start();
 
-Task tarea2 = new Task(() =>
-{
-    for (int j = 0; j < 100; j++)
-    {
-        var miThread = Thread.CurrentThread.ManagedThreadId;
+Task tarea = Task.Run(() => EjecutarTarea());
+Task tarea2 = tarea.ContinueWith(EjecutarOtraTarea);
 
-        Thread.Sleep(1000);
+//Task tarea2 = new Task(() =>
+//{
+//    for (int j = 0; j < 10; j++)
+//    {
+//        var miThread = Thread.CurrentThread.ManagedThreadId;
 
-        Console.WriteLine("Tarea correspondiente al hilo: "+miThread+ " ejecutandose desde el main");
-    }
-});
+//        Thread.Sleep(1000);
 
-tarea2.Start();
+//        Console.WriteLine("Tarea correspondiente al hilo: "+miThread+ " ejecutandose desde el main");
+//    }
+//});
+
+//tarea2.Start();
 
 Console.ReadLine();
 
 static void EjecutarTarea()
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 10; i++)
     {
         var miThread = Thread.CurrentThread.ManagedThreadId;
 
         Thread.Sleep(1000);
 
         Console.WriteLine("Esta vuelta de bucle corresponde al Thread: "+miThread);
+    }
+}
+
+static void EjecutarOtraTarea(Task obj)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        var miThread = Thread.CurrentThread.ManagedThreadId;
+
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Esto Es otra tarea y esta vuelta de bucle corresponde al Thread: " + miThread);
     }
 }
